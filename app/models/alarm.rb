1a77501dd35aa3b1de369940ffa525fb29baa383
jamst <jamstjamst@163.com>
2018-06-06 11:50:30 +0800

    多条告警触发

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,23 +1,30 @@
 class Alarm < ApplicationRecord
-   has_many :alarm_records	
+   has_many :alarm_records  
    has_many :alarm_details
-   include Activeable	
+   include Activeable 
 
 
-   class << self	
-	   # 5分钟粒度任务
-	   def crontab_5_minute
-	     where(time_gran:5).actived.each do |_|
-	       _.try(_.action_name)
-	     end
-	   end
+   class << self  
+     # 5分钟粒度任务
+     def crontab_5_minute
+       where(time_gran:5).actived.each do |_|
+         _.try(_.action_name)
+       end
+     end
 
-	   # 小时粒度任务
-	   def crontab_60_minute
-	     where(time_gran:60).actived.each do |_|
-	       _.try(_.action_name)
-	     end
-	   end
+     # 小时粒度任务
+     def crontab_60_minute
+       where(time_gran:60).actived.each do |_|
+         _.try(_.action_name)
+       end
+     end
+
+     # crontab_5_pm
+     def crontab_5_pm
+        where(time_gran:-5).actived.each do |_|
+         _.try(_.action_name)
+       end
+     end
    end
 
    # ************ 定义具体任务 ************
@@ -25,14 +32,39 @@ class Alarm < ApplicationRecord
     # 通用告警算法告警
     def default_action_name
        alarm_records.each do |_|
-         _.a = (_.kpi_vc.to_s.include? "@@") ?  eval(_.kpi_vc.strip.gsub("@@","")) : _.kpi_vc
-         _.b = (_.kpi_vn.to_s.include? "@@") ?  eval(_.kpi_vn.strip.gsub("@@","")) : _.kpi_vn
-         if _.kpi_method_execute 
-            _.update(alarm_status:1) 
-            AlarmDetail.create(alarm_id:_.alarm_id, alarm_record_id:_.id, color:_.color, describe:_.describe) 
-         else
-            _.update(alarm_status:0)
-         end   
+        if self.show_type == 0
+          # 多条告警模式
+          # kpi_vn = @@{"a&b"=>1,"b&c"=>2}
+          # Describe = #{@base_0}你好#{@base_1}
+          base_b = (_.kpi_vn.to_s.include? "@@") ?  eval(_.kpi_vn.strip.gsub("@@","")) : _.kpi_vn
+          base_b.to_a.each do |bb|
+             _.a = (_.kpi_vc.to_s.include? "@@") ?  eval(_.kpi_vc.strip.gsub("@@","")) : _.kpi_vc
+             _.b = bb[1]
+             if _.kpi_method_execute 
+                _.update(alarm_status:1) 
+                bb[0].split("&").each_with_index do |bb_s,index|
+                  eval %Q( @base_#{index} = '#{bb_s}' )
+                end
+                des = eval %Q( %Q(#{_.describe}) ) 
+                AlarmDetail.create(alarm_id:_.alarm_id, alarm_record_id:_.id, color:_.color, describe:des) 
+                WhUtils::set_employee_message(1,"#{des}","/admin/alarm/records",1) unless Notification.where("body = '#{_.describe}' and read_at is null").present?
+             else
+                _.update(alarm_status:0)
+             end   
+          end   
+        else
+           # 同类型告警只显示一条
+           _.a = (_.kpi_vc.to_s.include? "@@") ?  eval(_.kpi_vc.strip.gsub("@@","")) : _.kpi_vc
+           _.b = (_.kpi_vn.to_s.include? "@@") ?  eval(_.kpi_vn.strip.gsub("@@","")) : _.kpi_vn
+           if _.kpi_method_execute 
+              _.update(alarm_status:1) 
+              des = eval %Q( %Q(#{_.describe}) ) 
+              AlarmDetail.create(alarm_id:_.alarm_id, alarm_record_id:_.id, color:_.color, describe:des) 
+              WhUtils::set_employee_message(1,"#{des}","/admin/alarm/records",1) unless Notification.where("body = '#{_.describe}' and read_at is null").present?
+           else
+              _.update(alarm_status:0)
+           end
+        end
        end
     end
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,17 @@ set :output, { error: 'log/whenever_error.log', standard: 'log/whenever.log' }
 job_type :rake,  "export PATH=\"$HOME/.rbenv/bin:$PATH\" ; eval \"$(rbenv init -)\"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 job_type :runner, "export PATH=\"$HOME/.rbenv/bin:$PATH\" ; eval \"$(rbenv init -)\"; cd :path &&  RAILS_ENV=:environment bundle exec rails runner ':task' :output"
 
+# 每5分钟一次 自动告警监控
+every 60.minutes do
+  runner 'Alarm.crontab_5_minute'
+end
+
 # 每60分钟一次 自动告警监控
 every 60.minutes do
   runner 'Alarm.crontab_60_minute'
+end
+
+#每天下午5.00点
+every 1.day, :at => '05:00 pm' do
+  runner 'Alarm.crontab_5_pm'
 end
